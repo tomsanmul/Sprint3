9e0108aee7982b2d7d77e8e44a16618d57d6bab8
Tomas Sans <tomsan@gmail.com>
2022-11-10 12:10:29 +0100

    Canvis

---
+++ Entrega 3.1/Nivell 2/jugador.js
@@ -0,0 +1,12 @@
+class Jugador {
+    constructor (nom) {
+        let jugador = {
+            "userId": this.userId(),
+            "Nom": nom
+        };
+
+        this.jugador = jugador
+        return jugador
+    }
+
+}
