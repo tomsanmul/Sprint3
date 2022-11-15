bba70c8bfb0d111782943d19ca4fd75e3ffaf2fa
Tomas Sans <tomsan@gmail.com>
2022-11-15 13:22:37 +0100

    Canvis

--- Entrega 3.2/Nivell 1/app.js
+++ Entrega 3.2/Nivell 1/app.js
@@ -1,25 +1,12 @@
 //const Middleware = require('./middleware.js');
+const calculadora = require('./calculadora.js');
 const fs = require('fs');
 
-let datos = JSON.parse(fs.readFileSync('C:/Users/formacio/Desktop/Tomas/Sprint3/Entrega 3.2/Nivell 1/fitxer.json'));
-console.log(datos);
+let dades = require("./fitxer.json")
+console.log(dades);
 
-function suma(numero1, numero2){
-    return (numero1 + numero2);
-}
 
-function resta(numero1, numero2){
-    return (numero1 - numero2);
-}
+calculadora.suma(dades.numero1, dades.numero2);
+calculadora.resta(dades.numero1, dades.numero2);
+calculadora.multiplica(dades.numero1, dades.numero2);
 
-function multiplica(numero1, numero2){
-    return (numero1 * numero2);
-}
-
-let resultat;
-resultat = suma(datos.numero1, datos.numero2);
-console.log(`La SUMA de ${datos.numero1} + ${datos.numero2} es ${resultat}`);
-resultat = resta(datos.numero1, datos.numero2);
-console.log(`La RESTA de ${datos.numero1} - ${datos.numero2} es ${resultat}`);
-resultat = multiplica(datos.numero1, datos.numero2);
-console.log(`La MULTIPLICACIÓ de ${datos.numero1} * ${datos.numero2} es ${resultat}`);
---
+++ Entrega 3.2/Nivell 1/calculadora.js
@@ -0,0 +1,15 @@
+class Calculadora {
+
+suma(numero1, numero2){
+    console.log(`La SUMA de ${numero1} + ${numero2} es ${numero1 + numero2}`);
+}
+
+resta(numero1, numero2){
+    console.log(`La RESTA de ${numero1} - ${numero2} es ${numero1 - numero2}`);
+}
+
+multiplica(numero1, numero2){
+    console.log(`La MULTIPLICACIÓ de ${numero1} * ${numero2} es ${numero1 * numero2}`);
+}
+
+}
