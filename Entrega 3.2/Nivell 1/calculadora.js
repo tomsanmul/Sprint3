6295ab5502415bdf3012263ec51bad99a55222c3
tomsanmul <tomsan@gmail.com>
2022-11-15 22:07:29 +0100

    Canvis

--- Entrega 3.2/Nivell 1/calculadora.js
+++ Entrega 3.2/Nivell 1/calculadora.js
@@ -1,15 +1,15 @@
 class Calculadora {
 
-suma(numero1, numero2){
-    console.log(`La SUMA de ${numero1} + ${numero2} es ${numero1 + numero2}`);
-}
+    suma(numero1, numero2) {
+        console.log(`La SUMA de ${numero1} + ${numero2} es ${numero1 + numero2}`);
+    }
 
-resta(numero1, numero2){
-    console.log(`La RESTA de ${numero1} - ${numero2} es ${numero1 - numero2}`);
-}
+    resta(numero1, numero2) {
+        console.log(`La RESTA de ${numero1} - ${numero2} es ${numero1 - numero2}`);
+    }
 
-multiplica(numero1, numero2){
-    console.log(`La MULTIPLICACIÓ de ${numero1} * ${numero2} es ${numero1 * numero2}`);
-}
+    multiplica(numero1, numero2) {
+        console.log(`La MULTIPLICACIÓ de ${numero1} * ${numero2} es ${numero1 * numero2}`);
+    }
 
 }
