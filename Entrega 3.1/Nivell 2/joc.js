3234321f2cd0072a2f823244f1b99f9f26d61eab
Tomas Sans <tomsan@gmail.com>
2022-11-16 10:06:23 +0100

    Canvis

--- Entrega 3.1/Nivell 2/joc.js
+++ Entrega 3.1/Nivell 2/joc.js
@@ -9,11 +9,11 @@ class Joc {
 
     afegirJugador(j1, j2, j3, j4) {
         this.jugadors.push(j1, j2, j3, j4);
-        console.log(`Jugadors afegits al ${this.nom}: ${this.jugadors[0].nom} / ${this.jugadors[1].nom} / ${this.jugadors[2].nom} / ${this.jugadors[3].nom}.`);
+        console.log(`Jugadors afegits al joc '${this.nom}': ${this.jugadors[0].nom} / ${this.jugadors[1].nom} / ${this.jugadors[2].nom} / ${this.jugadors[3].nom}.`);
     }
 
     mostrarResultats(jugadors) {
-        console.log("\nPUNTS TOTALS DE CADA JUGADOR:")
+        console.log("\nPUNTS TOTALS DE CADA JUGADOR:\n")
         jugadors.forEach(jugador => {
             Marcador.mostrarPunts(jugador);
         });
