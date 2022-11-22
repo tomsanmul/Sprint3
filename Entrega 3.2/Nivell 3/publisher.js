e0e2c0de1d1e5dc10c6114f32b7d061f84af2558
Tomas Sans <tomsan@gmail.com>
2022-11-22 10:14:54 +0100

    Canvis

--- Entrega 3.2/Nivell 3/publisher.js
+++ Entrega 3.2/Nivell 3/publisher.js
@@ -1,18 +1,19 @@
 //Sol.licitem la biblioteca RabbitMQ. Sino está, la instalem "npm install amqplib"
-const { connect } = require('amqplib');
+const {
+  connect
+} = require('amqplib');
+
 const amqp = require('amqplib/callback_api');
 
 
-amqp.connect('amqp://localhost', function(error0, connection) {  //Ens conectem al servidor RabbitMQ
+amqp.connect('amqp://localhost', function(error0, connection) {
   if (error0) {
     throw error0;
   }
-  connection.createChannel(function(error1, channel) {  //Creem un canal, que eson resideix la major part de la API per fer les coses
+  connection.createChannel(function(error1, channel) {
     if (error1) {
       throw error1;
     }
-
-    //Per enviar, hem de declarar una cua per enviar a; llavor podrem publicar un misstage a la cua
     var queue = 'hello';
     var msg = 'Hello world';
 
@@ -23,13 +24,4 @@ amqp.connect('amqp://localhost', function(error0, connection) {  //Ens conectem 
     channel.sendToQueue(queue, Buffer.from(msg));
     console.log(" [x] Sent %s", msg);
   });
-
-  
-  setTimeout(function() {
-    connection.close();
-    process.exit(0)
-    }, 500);
-
 });
-
-
--- Entrega 3.2/Nivell 3/subscriber.js
+++ Entrega 3.2/Nivell 3/subscriber.js
@@ -1,11 +1,28 @@
-class Subscriber{
-    constructor(remitent, destinatari, misstage){
-            this.remitent = remitent;
-            this.destinatari = destinatari;
-            this.misstage = misstage;
+#!/usr/bin/env node
+
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
     }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
 
-}
+        var queue = 'hello';
 
-module.exports = Subscriber;
+        channel.assertQueue(queue, {
+            durable: false
+        });
 
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+        channel.consume(queue, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {
+            noAck: true
+        });
+    });
+});
