fd95ad62309b18820ff4866610059b0effaaa126
Tomas Sans <tomsan@gmail.com>
2022-11-23 09:28:38 +0100

    Canvis

--- Entrega 3.2/Nivell 1/app.js
+++ Entrega 3.2/Nivell 1/app.js
@@ -10,18 +10,28 @@ const mdw = new Middleware(calculadora,a,b)
 
 //Quadrat
 mdw.use((a, next) => {
-  console.log(`El cuadrat de ${a} es = ${a * 2}`);
+  let cuadrat = Math.pow(a, 2)
+  console.log(`El cuadrat de ${a} es = ${cuadrat}`);
   next();
 });
 
 
 //Cub
 mdw.use((req, next) => {
+  let cub = Math.pow(req, 3)
+  console.log(`El cub de ${a} es = ${cub}`);
+  next();
+});
+
+//Divisio
+mdw.use((req, next) => {
   req.b = req.b * 3;
+  console.log(`La divisio de ${a} ente ${b} es = ${(a / b).toFixed(2)}`);
   next();
 });
 
 
+
 mdw.suma(a,b);
-mdw.resta(a,b);
-mdw.multiplica(a,b);
+//mdw.resta(a,b);
+//mdw.multiplica(a,b);
