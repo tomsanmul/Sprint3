439459e13d8888063ba3487797c1b2bfe084a0ed
tomsanmul <tomsan@gmail.com>
2023-01-09 13:01:18 +0100

    Canvis

---
+++ exercicis.js
@@ -0,0 +1,34 @@
+//---------------Exercici 1
+function capitalAfterVowel(cadena){
+
+    let cadenafinal="";
+
+    let vocales = ['a','e','i','o','u',' '];
+
+
+    for(i=0;i< cadena.length;i++){
+
+        if ((vocales.indexOf(cadena[i]) == -1) && (vocales.indexOf(cadena[i-1] > 0))){   //Si el caracter actual es consonante y el anterior es vocal, hacemos uppercase
+            
+            cadenafinal += cadena[i].toUpperCase();
+        }
+        else
+        {
+            cadenafinal += cadena[i];
+        }
+
+    }
+
+    console.log(cadenafinal);
+}
+
+capitalAfterVowel("hello world");
+capitalAfterVowel("xaaBeuekadii");
+
+
+
+
+
+//----------------Exercici 2
+
+function sumEveryOther
