db7e6324ab1ad90cb614427963878d2ef089e367
tomsanmul <tomsan@gmail.com>
2023-01-10 00:31:55 +0100

    Canvis

--- exercicisExtras.js
+++ exercicisExtras.js
@@ -1,4 +1,4 @@
-//---------------Exercici 1
+//-------------------------Exercici 1
 //Given a string, make every consonant after a vowel uppercase.
 //Can you do this and without regex?
 
@@ -15,10 +15,9 @@ function capitalAfterVowel(cadena) {
         if ((vocales.indexOf(cadena[i]) == -1) && (vocales.indexOf(cadena[i - 1] > 0))) {
 
             cadenafinal += cadena[i].toUpperCase();
-        } 
-         //sino, copiem tal cual la lletra
-        else
-        {
+        }
+        //sino, copiem tal cual la lletra
+        else {
             cadenafinal += cadena[i];
         }
 
@@ -32,7 +31,7 @@ capitalAfterVowel("xaaBeuekadii");
 
 
 
-//----------------Exercici 2
+//-------------------------Exercici 2
 //Given a number, sum every second digit in that number.
 
 function sumEveryOther(numero) {
@@ -50,10 +49,51 @@ sumEveryOther(1010.11);
 
 
 
-//----------------Exercici 3
+//--------------------------Exercici 3
 //Given an array of integers arr and an integer n, return a subarray of arr of
 //length n where the sum is the largest. Make sure you maintain the order of the
 //original array, and if n is greater than arr.length, you can choose what you want to return
+
+function maxSubArray(arrayOriginal, posicion) {
+
+    let sumaValores = 0,
+        sumaMaxima = 0;
+
+    let i = 0,
+        j = 0,
+        vueltas = arrayOriginal.length - posicion + 1;
+
+    let arrayDefinitivo = [],
+        arrayTemporal = [];
+
+    if (posicion > arrayOriginal.length) {
+        console.log("No se puede dedolver un subArray mas grande que el original.");
+        return;
+    } else {
+
+        for (i = 0; i < vueltas; i++) {
+
+            for (j = i; j < (posicion + i); j++) {
+                sumaValores = +arrayOriginal[j];
+                arrayTemporal.push(arrayOriginal[j]);
+            }
+
+            if (sumaValores > sumaMaxima) {
+                sumaMaxima = sumaValores;
+                arrayDefinitivo = arrayTemporal;
+            }
+
+            //borramos el array y la sumaValores, y vuelta a empezar
+            sumaValores = 0;
+            arrayTemporal = [];
+        }
+    }
+    console.log(arrayDefinitivo);
+    return;
+}
+
+maxSubArray([-4, 2, -5, 1, 2, 3, 6, -5, 1], 4);
+maxSubArray([1, 2, 0, 5], 2);
 
 
 
