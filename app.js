6f060878950208373ab68b3c7b47eec7081924e1
tomsanmul <tomsan@gmail.com>
2023-01-11 02:20:20 +0100

    Canvis

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const Pokemon = require("./pokemon.js");
 
-
+//declaro objetos de Ataques, (At1, At2, At3, At4),  per despres ficarlos en el arry d' atacs directament a cada Pokemon)
 let At1 = new Object();
-    At1.nom = 'Ataque relamapgo';
+    At1.nombre = 'Ataque relamapgo';
     At1.potencia = 3;
 
 let At2 = new Object();
-    At2.nom = 'Ataque freidor';
+    At2.nombre = 'Ataque freidor';
     At2.potencia = 4;
 
 let At3 = new Object();
-    At3.nom = 'Ataque acuatico';
+    At3.nombre = 'Ataque acuatico';
     At3.potencia = 2;
 
 let At4 = new Object();
-    At4.nom = 'Ataque electrico';
+    At4.nombre = 'Ataque electrico';
     At4.potencia = 1;
     
 
 
 let pokemons = [
-    new Pokemon ('1', 'Tomas',3,'fuego',[At1, At2, At3, At4]), 
+    new Pokemon ('1', 'Tomas',3,'fuego',[At1, At3]), 
     new Pokemon ('2', 'Boris',2,'agua',[At1, At2, At4]), 
     new Pokemon ('3', 'Ivan',1,'electrico',[At1, At3, At4]), 
     new Pokemon ('4', 'Marta',5,'agua',[At2, At3]), 
@@ -29,31 +29,51 @@ let pokemons = [
 
 ];
 
+//Exercici 1: Força im Error si el Tipus es diferent a agua, fuego, electrico, planta
+console.log("\nExercici 1: Força un Error si el Tipus es diferent a agua, fuego, electrico, planta\n");
+try {
+    new Pokemon ('7', 'Jaime',5,'arbol', [At1, At2]);
+  } 
+  catch (error) {
+    console.log('Error: ' + error.message);
+  }
 
-//Mostrar el nom i numero dels Pokemons electrics
 
-let electrics = pokemons.find((pokemon) => pokemon.tipo == "electrico");
-console.log(electrics);
+// Exercici 2: Mostra tots els Pokemons
+console.log("\nExercici 2: Mostra tots els Pokemons\n");
+console.table(pokemons);
 
 
+//Exercici 3: Mostrar el nom i numero dels Pokemons electrics
+console.log("\nExercici 3: Mostrar el nom i numero dels Pokemons electrics: \n");
 
-//Mostrar el listado de ataques de cada pokemon 
+for(i=0;i<pokemons.length; i++){
+      if (pokemons[i].tipo === 'electrico') {
+        console.log(`Numero: ${pokemons[i].numero} || Nom: ${pokemons[i].nombre}`);
+      }
+    };
 
+
+//Exercici 4: Mostrar el llistat d'atacs de cada pokemon 
+console.log("\nEx 4: Mostrar el llista d'atacs de cada pokemon: \n");
 for(i=0; i<pokemons.length;i++){
 
-    console.log(`Nom Pokemon: ${pokemons[i].nombre}`);
-
-    console.log(pokemons[i].ataque.length);
+    console.log(`Nombre Pokemon: ${pokemons[i].nombre}`);
 
     for(j=0; j < pokemons[i].ataque.length;j++){
-        console.log(`Ataque Nombre: ${pokemons[i].ataque.nom}`);
-        console.log(`Ataque potencia: ${pokemons[i].ataque.potencia}`);
+        console.log(`Ataque ${j+1} ||  Nombre: ${pokemons[i].ataque[j].nombre}  ||  Potencia: ${pokemons[i].ataque[j].potencia}`);
     }
+    console.log("");
 }
 
 
-/*
-for(i=0; i<pokemons.length;i++){
-    console.log(pokemons);
-}
-*/
+//Exercici 5: Als Pokemon del tipus aigua els hi sumem 2 nivells: 
+console.log("\nExercici 5: Als Pokemon del tipus aigua els hi sumem 2 nivells: \n");
+
+for(i=0;i<pokemons.length; i++){
+      if (pokemons[i].tipo === 'agua') {
+        pokemons[i].subirNivel(2);
+      }
+    };
+// Motrem novament els Pokemos para veure el cambio 
+console.table(pokemons);
